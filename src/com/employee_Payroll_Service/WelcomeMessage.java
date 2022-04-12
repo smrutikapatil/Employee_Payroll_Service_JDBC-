21d396c0249d979673e459fd070de6454a9ce590
smrutikapatil <smruti.patil17@gmail.com>
2022-04-12 20:53:35 +0530

    Added- Created Payroll Service Database

--- src/com/employee_Payroll_Service/WelcomeMessage.java
+++
@@ -1,9 +0,0 @@
-package com.employee_Payroll_Service;
-
-    public class WelcomeMessage {
-        public static void main(String[] args) {
-            System.out.println("Welcome to employee payroll service");
-        }
-    }
-
-
