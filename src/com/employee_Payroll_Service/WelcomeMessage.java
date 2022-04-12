fd38a344cc102fcbdbdd094c8bceec596ca46dd8
smrutikapatil <smruti.patil17@gmail.com>
2022-04-12 20:48:08 +0530

    first commit

---
+++ src/com/employee_Payroll_Service/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package com.employee_Payroll_Service;
+
+    public class WelcomeMessage {
+        public static void main(String[] args) {
+            System.out.println("Welcome to employee payroll service");
+        }
+    }
+
+
